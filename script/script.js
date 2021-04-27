f8e58d8fb7ae3ee84858f01aee36f70d813df1c4
nicolas <79875546+Nicolas-beginner@users.noreply.github.com>
2021-04-27 16:55:14 -0300

    tratamento de erros com catch e animação de load

--- script/script.js
+++ script/script.js
@@ -7,6 +7,7 @@ var btn = document.querySelector(".btnComprar")
 var menu = document.querySelector(".opcaoMenu")
 var produtoDetailMain = document.querySelector(".produtoDetail")
 var search = document.querySelector(".search")
+var load = document.querySelector(".loader")
 
 /*variaveis do cep*/ 
 var cep = document.querySelector('#cep')
@@ -18,6 +19,10 @@ var cidade = document.querySelector('#cidade')
 var logradouro = document.querySelector("#logradouro")
 var cepContainer = document.querySelector(".cepContainer")
 cepContainer.classList.add("remove")
+
+function loaderoff(){
+    load.classList.remove("loader")
+}
 
 
 function zeraOsCampos(){
@@ -66,9 +71,21 @@ function buscaJson(){
 
     .then(function (response){
 
-   return response.json()
+    if(!response.ok){
 
-    }).then(function(json){
+        throw Error(response.statusText)
+    
+}
+        return response.json()
+
+    })
+
+        .catch(e => {
+            
+            console.log("deu pau", e)
+    
+        })
+    .then(function(json){
         console.log(json)
         cep.value = json.cep
         estado.value = json.uf
@@ -86,15 +103,26 @@ function buscaJson(){
 
 fetch("https://fakestoreapi.com/products")
 
+
+
 .then(function(response){
     console.log(response)
-    return response.json()
+    
+    if(response.ok != true){
+        throw Error(response.status)
+    }
+    else{
+        return response.json()
+
+    }
+})
+.catch(erro => {
+    console.log("conexão instavel ou perda de pacotes tente novamente mais tarde", erro)
 })
 
 .then(function(keys){
   Array.from(keys).forEach(key => {
         var div = document.createElement('div')   
-       
         div.innerHTML = `
         <img src="`+key.image+`" alt="" class="imagemProduto">
         <p class="tituloProduto">`+key.title+`<p>
@@ -102,10 +130,11 @@ fetch("https://fakestoreapi.com/products")
         <p class="preco">`+key.price+` R$<p>
         <button class="btnComprar" id="btnComprar">COMPRAR</button>
         `   
-        
+        loaderoff()
         main.appendChild(div)
         div.classList.add("elementsPrincipalMain")
         div.addEventListener("click", call => {
+            
             carregaProdutoDetail(key.id)
         })
 
@@ -119,12 +148,18 @@ function carregaProdutoDetail(id){
 fetch(`https://fakestoreapi.com/products/`+id+``)
 
 .then(response => {
-
+  console.log(response)
+    if(!response.ok){
+        throw Error(response.status)
+    }
     return response.json()
 
 })
+.catch(statusCode => {
+    console.log(statusCode, "conexão instavel ou perda de pacotes tente novamente mais tarde")
+})
 .then(json =>{
-
+    
     cepContainer.classList.remove("remove")
     cepContainer.classList.add("cepContainer")
 
@@ -136,13 +171,14 @@ fetch(`https://fakestoreapi.com/products/`+id+``)
 
 
     var productDetail = document.createElement("div")
-
     productDetail.innerHTML = `
+        
         <img src="`+json.image+`" alt="" class="imagemProdutoDetail">
         <p class="tituloDetail">`+json.title+`<p>
         <p class="descricaoDetail">`+json.description+`<p>
         <p class="precoDetail">`+json.price+` R$<p>
         <button class="btnComprarDetail" id="btnComprar">COMPRAR</button>
+
         ` 
         
         produtoDetailMain.appendChild(productDetail)
